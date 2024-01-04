9992169f234d5a0a3f2ae58f75967b4781c67424
Varad Sharma <varad.code19@gmail.com>
2024-01-04 18:25:53 +0530

    init

---
+++ lua/plugins/core.lua
@@ -0,0 +1,32 @@
+return {
+  { "andweeb/presence.nvim" },
+  {
+    "catppuccin/nvim",
+
+    require("catppuccin").setup({
+      flavor = "mocha",
+      transparency = false,
+      dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+    }),
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
